758acb8090383cf25ec6ea707345c10de28f4572
djo00771 <djo00771@mail.ru>
2022-06-01 18:11:50 +0300

    update file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,73 +1,56 @@
 import pytest
 from app.calculator import Calculator
 from datetime import datetime
+from app.settings import positive_data_division, positive_data_multiply,\
+    positive_data_subtraction, positive_data_adding, negative_data_division, \
+    negative_data_multiply, negative_data_subtraction, negative_data_adding
 
 
 class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-# Позитивные тесты -------------------------------------------------------------------------------------
+# Позитивные тесты 16 кейсов --------------------------------------------------------------------------
+    '''Строка с переменными и список кортежей с данными, которые мы передаем в переменные'''
 
-    @pytest.mark.parametrize('x, y , expected_result', [(10, 2, 5),
-                                                        (20, 10, 2),
-                                                        (30, -3, -10),
-                                                        (5, 2, 2.5)])
+    @pytest.mark.parametrize('x, y , expected_result', positive_data_division)
     def test_division_good(self, x, y, expected_result):
         assert self.calc.divizion(self, x, y) == expected_result
 
-    @pytest.mark.parametrize('x, y , expected_result', [(10, 2, 20),
-                                                        (20, 10, 200),
-                                                        (30, -3, -90),
-                                                        (5, 2.5, 12.5)])
+    @pytest.mark.parametrize('x, y , expected_result', positive_data_multiply)
     def test_multiply_good(self, x, y, expected_result):
         assert self.calc.multiply(self, x, y) == expected_result
 
-    @pytest.mark.parametrize('x, y , expected_result', [(10, 2, 8),
-                                                        (20, 10, 10),
-                                                        (30, -3, 33),
-                                                        (5, 2.5, 2.5)])
+    @pytest.mark.parametrize('x, y , expected_result', positive_data_subtraction)
     def test_subtraction_good(self, x, y, expected_result):
         assert self.calc.substraction(self, x, y) == expected_result
 
-    @pytest.mark.parametrize('x, y , expected_result', [(10, 2, 12),
-                                                        (20, 10, 30),
-                                                        (30, -3, 27),
-                                                        (5, 2.5, 7.5)])
+    @pytest.mark.parametrize('x, y , expected_result', positive_data_adding)
     def test_adding_good(self, x, y, expected_result):
         assert self.calc.adding(self, x, y) == expected_result
 
-# Негативные тесты ------------------------------------------------------------------------------------
+# Негативные тесты 14 кейсов -------------------------------------------------------------------------
+    '''Строка с переменными и список кортежей с данными, которые мы передаем в переменные'''
 
-    @pytest.mark.parametrize('expected_exception, divider, divisible', [(ZeroDivisionError, 0, 10),
-                                                                        (TypeError, '2', 20)])
+    @pytest.mark.parametrize('expected_exception, divider, divisible', negative_data_division)
     def test_division_with_error(self, expected_exception, divider, divisible):
         with pytest.raises(expected_exception):
             self.calc.divizion(self, divisible, divider)
 
-    @pytest.mark.parametrize('x, y , expected_result', [(10, 0, 22),
-                                                        (20, 10, 0),
-                                                        (30, -3, -1),
-                                                        (5, 2.5, 0)])
+    @pytest.mark.parametrize('x, y , expected_result', negative_data_multiply)
     def test_multiply_error(self, x, y, expected_result):
         assert self.calc.multiply(self, x, y) != expected_result
 
-    @pytest.mark.parametrize('x, y , expected_result', [(10, 2, 0),
-                                                        (20, 10, 0),
-                                                        (30, -3, 0),
-                                                        (5, 2.5, 0)])
+    @pytest.mark.parametrize('x, y , expected_result', negative_data_subtraction)
     def test_subtraction_error(self, x, y, expected_result):
         assert self.calc.substraction(self, x, y) != expected_result
 
-    @pytest.mark.parametrize('x, y , expected_result', [(10, 2, 0),
-                                                        (20, 10, 0),
-                                                        (30, -3, 0),
-                                                        (5, 2.5, 0)])
+    @pytest.mark.parametrize('x, y , expected_result', negative_data_adding)
     def test_adding_error(self, x, y, expected_result):
         assert self.calc.adding(self, x, y) != expected_result
 
 
-# Таймер -----------------------------------------------------------------------------------------------
+# Таймер ---------------------------------------------------------------------------------------------
 
 # @pytest.fixture(autouse=True)
 # def time_delta():
